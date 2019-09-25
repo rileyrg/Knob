05a7543cddd23bd642cf28e88eb2f6ba62072a74
Richard Riley <rileyrg@gmail.com>
2019-09-25 21:14:20 +0200

    single pulse

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,25 @@
 
 Servo myservo;  // create servo object to control a servo
 int potpin = 0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
+int pot,servoVal;    // variable to read the value from the analog pin
+char buffer[1024];
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+     Serial.begin(9600);
+     myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
+
+bool fFirstRun=true;
+
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+     // pot = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+     // servoVal = map(pot, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+     // myservo.write(servoVal);                  // sets the servo position according to the scaled value
+     // sprintf(buffer,"Potentiometer value (0-1023) is: %d. Servo value is %d.",pot,servoVal);
+     // Serial.println(buffer);
+     if(fFirstRun){
+          myservo.write(00);                  // sets the servo position according to the scaled value
+          fFirstRun=false;
+     }
 }
 
